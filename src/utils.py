7002c2dabfb7493eba29543fa96c3073a8774c61
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-20 15:20:08 +0900

    add load_file()

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
+import json
+import gzip
 from dataclasses import dataclass
-from typing import Any
+from pathlib import Path
+from typing import Any, Union
 
 
 @dataclass
@@ -14,3 +17,16 @@ class Example:
     @classmethod
     def from_json(cls, json_obj: dict[str, Any]):
         return cls(**json_obj)
+
+
+def load_file(path: Union[str, Path]) -> list[Example]:
+    """Load a file containing examples in JSON format.
+
+    Args:
+        path (Union[str, Path]): Path to the file.
+
+    Returns:
+        list[Example]: List of examples.
+    """
+    with gzip.open(path, "rt") as f:
+        return [Example.from_json(json.loads(line)) for line in f]
