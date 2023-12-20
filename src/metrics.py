7d62bdc24c637c41490c243fb25361db5d8883db
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-20 17:23:14 +0900

    add min_k_percent_probability()

--- src/metrics.py
+++ src/metrics.py
@@ -14,4 +14,34 @@ def perplexity(logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
     """
     loss_fct = CrossEntropyLoss(reduction="none")
     cross_entropy = loss_fct(logits.transpose(1, 2), labels)
-    return torch.exp(cross_entropy.sum(1) / labels.shape[1])
+    perplexity = torch.exp(cross_entropy.sum(1) / labels.shape[1])
+    return perplexity
+
+
+def min_k_percent_probability(
+    logits: torch.Tensor,
+    labels: torch.Tensor,
+    k: float = 20.0,
+) -> torch.Tensor:
+    """Calculate Min-K % Prob in Shi et al. (2023).
+
+    Args:
+        logits (torch.Tensor): Logits of shape (batch_size, sequence_length, vocab_size).
+        labels (torch.Tensor): Labels of shape (batch_size, sequence_length).
+        k (float, optional): K in Min-K % Prob. Defaults to 20.0.
+
+    Returns:
+        torch.Tensor: Min-K % Prob of shape (batch_size,).
+
+    References:
+        https://arxiv.org/abs/2310.16789
+    """
+    ll = torch.log_softmax(logits, dim=2)  # log probabilities
+    ll = ll.gather(2, labels.unsqueeze(2)).squeeze(2)  # log probabilities of labels
+
+    num_k_percent_tokens = int(labels.shape[1] * k / 100)
+    assert num_k_percent_tokens > 0, "k is too small"
+    ll, _ = torch.sort(ll, dim=1)
+    ll = ll[:, :num_k_percent_tokens]
+    min_k_percent_probability = ll.sum(1) / num_k_percent_tokens
+    return min_k_percent_probability
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,7 +2,7 @@ import unittest
 
 import torch
 
-from src.metrics import perplexity
+from src.metrics import perplexity, min_k_percent_probability
 
 
 class TestPerplexity(unittest.TestCase):
@@ -12,18 +12,70 @@ class TestPerplexity(unittest.TestCase):
                 [100.0, -100.0],
                 [100.0, -100.0],
                 [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
             ],
             [
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+            ],
+            [
+                [100.0, -100.0],
+                [100.0, -100.0],
                 [100.0, -100.0],
                 [100.0, -100.0],
                 [100.0, -100.0],
             ],
         ]
         labels = [
-            [0, 0, 0],
-            [1, 1, 1],
+            [0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1],
         ]
         perplexities = perplexity(torch.tensor(logits), torch.tensor(labels)).tolist()
-        self.assertEqual(len(perplexities), 2)
+        self.assertEqual(len(perplexities), 3)
         self.assertAlmostEqual(perplexities[0], 1.0)
-        self.assertNotAlmostEqual(perplexities[1], 1.0)
+        self.assertGreater(perplexities[1], 1.0)
+        self.assertGreater(perplexities[2], 1.0)
+
+
+class TestMinKPercentProbability(unittest.TestCase):
+    def test_min_k_percent_probability(self):
+        logits = [
+            [
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+            ],
+            [
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+            ],
+            [
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+                [100.0, -100.0],
+            ],
+        ]
+        labels = [
+            [0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+        ]
+        min_k_percent_probabilities = min_k_percent_probability(
+            torch.tensor(logits), torch.tensor(labels), k=20.0
+        ).tolist()
+        self.assertEqual(len(min_k_percent_probabilities), 3)
+        self.assertAlmostEqual(min_k_percent_probabilities[0], 0.0)
+        self.assertLess(min_k_percent_probabilities[1], 0.0)
+        self.assertLess(min_k_percent_probabilities[2], 0.0)
