2fd112aa478c28b047f5b1123629b567f0ba4a58
Hirokazu Kiyomaru <13678589+hkiyomaru@users.noreply.github.com>
2023-12-20 15:26:44 +0900

    Add utility functions to load data (#7)
    * add the Example class
    
    * add load_file()
    
    * add FOLDS and LOCAL_RANKS as global variables

---
+++ src/utils.py
@@ -0,0 +1,36 @@
+import json
+import gzip
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Union
+
+
+FOLDS = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "refined"]
+LOCAL_RANKS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+
+
+@dataclass
+class Example:
+    iteration: int
+    dataset_idx: int
+    dataset_name: str
+    doc_ids: list[int]
+    text: str
+    token_ids: list[int]
+
+    @classmethod
+    def from_json(cls, json_obj: dict[str, Any]):
+        return cls(**json_obj)
+
+
+def load_file(path: Union[str, Path]) -> list[Example]:
+    """Load a file containing examples in JSON format.
+
+    Args:
+        path (Union[str, Path]): Path to the file.
+
+    Returns:
+        list[Example]: List of examples.
+    """
+    with gzip.open(path, "rt") as f:
+        return [Example.from_json(json.loads(line)) for line in f]
---
+++ tests/test_utils.py
@@ -0,0 +1,68 @@
+import dataclasses
+import gzip
+import json
+import tempfile
+import unittest
+
+from src.utils import Example, load_file
+
+
+class TestExample(unittest.TestCase):
+    def test_example(self):
+        example = Example(
+            iteration=0,
+            dataset_idx=0,
+            dataset_name="test",
+            doc_ids=[0],
+            text="test",
+            token_ids=[0],
+        )
+        self.assertEqual(example.iteration, 0)
+        self.assertEqual(example.dataset_idx, 0)
+        self.assertEqual(example.dataset_name, "test")
+        self.assertEqual(example.doc_ids, [0])
+        self.assertEqual(example.text, "test")
+        self.assertEqual(example.token_ids, [0])
+
+    def test_example_from_json(self):
+        json_obj = {
+            "iteration": 0,
+            "dataset_idx": 0,
+            "dataset_name": "test",
+            "doc_ids": [0],
+            "text": "test",
+            "token_ids": [0],
+        }
+        example = Example.from_json(json_obj)
+        self.assertEqual(example.iteration, 0)
+        self.assertEqual(example.dataset_idx, 0)
+        self.assertEqual(example.dataset_name, "test")
+        self.assertEqual(example.doc_ids, [0])
+        self.assertEqual(example.text, "test")
+        self.assertEqual(example.token_ids, [0])
+
+
+class TestLoadFile(unittest.TestCase):
+    def test_load_file(self):
+        example = Example(
+            iteration=0,
+            dataset_idx=0,
+            dataset_name="test",
+            doc_ids=[0],
+            text="test",
+            token_ids=[0],
+        )
+        with tempfile.NamedTemporaryFile(mode="wb") as temp:
+            with gzip.open(temp.name, "wt") as f:
+                f.write(json.dumps(dataclasses.asdict(example)) + "\n")
+                f.write(json.dumps(dataclasses.asdict(example)) + "\n")
+                f.write(json.dumps(dataclasses.asdict(example)) + "\n")
+            temp.seek(0)
+            examples = load_file(temp.name)
+            self.assertEqual(len(examples), 3)
+            self.assertEqual(examples[0].iteration, 0)
+            self.assertEqual(examples[0].dataset_idx, 0)
+            self.assertEqual(examples[0].dataset_name, "test")
+            self.assertEqual(examples[0].doc_ids, [0])
+            self.assertEqual(examples[0].text, "test")
+            self.assertEqual(examples[0].token_ids, [0])
