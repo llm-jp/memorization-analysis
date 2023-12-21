ed9e010ab66dadde6e7f557063dcc621d20e6e1e
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-21 11:30:05 +0900

    add code to load examples

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,10 +1,33 @@
+import argparse
 import logging
+from pathlib import Path
+
+from utils import FOLDS, LOCAL_RANKS, load_examples
 
 logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        required=True,
+        help="The directory containing data files.",
+    )
+    args = parser.parse_args()
+
+    logger.info(f"Data directory: {args.data_dir}")
+    data_dir = Path(args.data_dir)
+
+    for fold in FOLDS:
+        for local_rank in LOCAL_RANKS:
+            data_file = (
+                data_dir / f"used_data_{fold}" / f"used_data_{local_rank}.jsonl.gz"
+            )
+            step_examples_map = load_examples(data_file)
+            logger.info(f"Loaded {len(step_examples_map)} steps from {data_file}.")
+            return
 
 
 if __name__ == "__main__":
