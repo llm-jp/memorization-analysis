f737dcad5858fd759068099742be3c6505e38a4e
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-21 11:20:39 +0900

    add evaluate.py

---
+++ src/evaluate.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(name)s:%(lineno)d: %(levelname)s: %(message)s",
+    )
+    main()
