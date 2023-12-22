84bbdbbc4cc0ba4191a8ce04000c738d28a24d73
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-22 14:36:07 +0900

    add save_examples()

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,8 @@
 import gzip
 import json
-from dataclasses import dataclass, field
+from dataclasses import asdict, dataclass, field
 from pathlib import Path
-from typing import Iterator, Union
+from typing import Iterable, Iterator, Union
 
 FOLDS = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "refined"]
 LOCAL_RANKS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
@@ -32,3 +32,15 @@ def load_examples(path: Union[str, Path]) -> Iterator[Example]:
     with gzip.open(path, "rt") as f:
         for line in f:
             yield Example(**json.loads(line))
+
+
+def save_examples(examples: Iterable[Example], path: Union[str, Path]) -> None:
+    """Save examples in JSON format.
+
+    Args:
+        examples (Iterable[Example]): An iterable over the examples.
+        path (Union[str, Path]): Path to the file.
+    """
+    with gzip.open(path, "wt") as f:
+        for example in examples:
+            f.write(json.dumps(asdict(example), ensure_ascii=False) + "\n")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ import tempfile
 import unittest
 from collections import defaultdict
 
-from src.utils import Example, load_examples
+from src.utils import Example, load_examples, save_examples
 
 
 class TestExample(unittest.TestCase):
@@ -71,6 +71,33 @@ class TestLoadExamples(unittest.TestCase):
                 f.write(json.dumps(dataclasses.asdict(example)) + "\n")
                 f.write(json.dumps(dataclasses.asdict(example)) + "\n")
                 f.write(json.dumps(dataclasses.asdict(example)) + "\n")
+            temp.seek(0)
+
+            step_examples_map = defaultdict(list)
+            for example in load_examples(temp.name):
+                step_examples_map[example.iteration].append(example)
+            self.assertEqual(len(step_examples_map), 1)
+            self.assertEqual(len(step_examples_map[0]), 3)
+            self.assertEqual(step_examples_map[0][0].iteration, 0)
+            self.assertEqual(step_examples_map[0][0].dataset_idx, 0)
+            self.assertEqual(step_examples_map[0][0].dataset_name, "test")
+            self.assertEqual(step_examples_map[0][0].doc_ids, [0])
+            self.assertEqual(step_examples_map[0][0].text, "test")
+            self.assertEqual(step_examples_map[0][0].token_ids, [0])
+
+
+class TestSaveExamples(unittest.TestCase):
+    def test_save_examples(self):
+        example = Example(
+            iteration=0,
+            dataset_idx=0,
+            dataset_name="test",
+            doc_ids=[0],
+            text="test",
+            token_ids=[0],
+        )
+        with tempfile.NamedTemporaryFile(mode="w") as temp:
+            save_examples([example, example, example], temp.name)
             temp.seek(0)
 
             step_examples_map = defaultdict(list)
