2c854d158646250633188ecdf02ef3dd78fdeeeb
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-12-22 17:55:44 +0900

    add main logic

--- src/plot.py
+++ src/plot.py
@@ -1,10 +1,36 @@
+import argparse
+import logging
 from collections import defaultdict
 from pathlib import Path
 from typing import Union
 
 import numpy as np
 import plotly.graph_objs as go
-from utils import Example
+from utils import Example, load_examples
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse command-line arguments.
+
+    Returns:
+        argparse.Namespace: The parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        required=True,
+        help="The directory containing data files.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        required=True,
+        help="The directory to save the output files.",
+    )
+    return parser.parse_args()
 
 
 def plot_perplexity(
@@ -160,3 +186,44 @@ def plot_extractable(
         yaxis_title="Sequence length",
     )
     fig.write_image(str(path))
+
+
+def main(args: argparse.Namespace) -> None:
+    logger.info(f"Load data from {args.data_dir}")
+    data_dir = Path(args.data_dir)
+
+    examples = []
+    for path in data_dir.glob("**/*.jsonl.gz"):
+        logger.info(f"Load examples from {path}.")
+        for example in load_examples(path):
+            examples.append(example)
+
+    logger.info(f"Create output directory {args.output_dir}")
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    logger.info("Plot perplexity.")
+    path = output_dir / "perplexity.pdf"
+    plot_perplexity(examples, path)
+    logger.info(f"Saved to {path}.")
+
+    logger.info("Plot min-k% probability.")
+    path = output_dir / "min_k_percent_prob.pdf"
+    plot_min_k_percent_prob(examples, path)
+    logger.info(f"Saved to {path}.")
+
+    logger.info("Plot extractable fraction.")
+    path = output_dir / "extractable.pdf"
+    plot_extractable(examples, path)
+    logger.info(f"Saved to {path}.")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.INFO,
+        format="%(asctime)s %(name)s:%(lineno)d: %(levelname)s: %(message)s",
+    )
+
+    main(args)
