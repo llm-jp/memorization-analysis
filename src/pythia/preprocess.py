f3e7e276c128ff6ae8aa579e92fcb6969ece83ff
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2024-01-31 11:19:10 +0900

    add extract_batch()

--- src/pythia/preprocess.py
+++ src/pythia/preprocess.py
@@ -3,10 +3,11 @@ import gzip
 import json
 import logging
 import os
+from typing import Any
 
 from mmap_dataset import MMapIndexedDataset
 from tqdm import trange
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, PreTrainedTokenizer
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +26,7 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, required=True, help="Path to the data directory")
     parser.add_argument("--output_path", type=str, required=True, help="Path to the output directory")
+    parser.add_argument("--chunk_size", type=int, default=1000, help="The number of steps per file")
     parser.add_argument(
         "--verbose",
         "-v",
@@ -34,39 +36,62 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main(args: argparse.Namespace) -> None:
-    # Create output directory if not exists
-    os.makedirs(args.output_path, exist_ok=True)
+def extract_batch(
+    dataset: MMapIndexedDataset,
+    tokenizer: PreTrainedTokenizer,
+    iteration: int,
+) -> list[dict[str, Any]]:
+    """Extract examples from the dataset.
 
+    Args:
+        dataset (MMapIndexedDataset): The dataset.
+        tokenizer (PreTrainedTokenizer): The tokenizer.
+        iteration (int): The number of iteration.
+
+    Returns:
+        list[dict[str, Any]]: The extracted examples.
+    """
+    batch_token_ids = dataset[iteration * BATCH_SIZE : (iteration + 1) * BATCH_SIZE]
+    batch_texts = tokenizer.batch_decode(batch_token_ids)
+    examples = []
+    for token_ids, text in zip(batch_token_ids, batch_texts):
+        example = {
+            "iteration": iteration,
+            "dataset_idx": 0,
+            "dataset_name": "pile",
+            "doc_ids": [0],
+            "text": text,
+            "token_ids": token_ids.tolist(),
+        }
+        examples.append(example)
+    return examples
+
+
+def main(args: argparse.Namespace) -> None:
+    logger.info(f"Load data from {args.data_path}")
+    dataset = MMapIndexedDataset(args.data_path, skip_warmup=True)
+
+    logger.info("Create tokenizer")
     tokenizer = AutoTokenizer.from_pretrained(
         "EleutherAI/pythia-14m",
-        revision="step3000",
-        cache_dir="/model/i-sugiura/pythia-14m/step3000",
+        revision="step3000",  # TODO: Do we need to specify this?
     )
-    dataset = MMapIndexedDataset(args.data_path, skip_warmup=True)
-    logger.info(len(dataset))
-    steps_per_file = 1000
-    for i in trange(0, 143):
-        path = os.path.join(args.output_path, f"pythia-{i*steps_per_file:05d}-{(i+1)*steps_per_file-1:05d}.jsonl.gz")
-        with gzip.open(path, "wt", encoding="utf-8") as output_file:
-            for j in trange(steps_per_file):
-                iteration = i * steps_per_file + j
-                current_file_lines = []
-                # TODO: end is (iteration+1)*1024 + 1 ? or not?
-                batch = dataset[iteration * BATCH_SIZE : (iteration + 1) * BATCH_SIZE]
-                for data in batch:
-                    text = tokenizer.decode(data)
-                    token_ids = data.tolist()
-                    formatted_data = {
-                        "iteration": iteration,
-                        "dataset_idx": 0,
-                        "dataset_name": "pile",
-                        "doc_ids": [0],
-                        "text": text,
-                        "token_ids": token_ids,
-                    }
-                    current_file_lines.append(json.dumps(formatted_data, ensure_ascii=False))
-                output_file.write("\n".join(current_file_lines))
+
+    logger.info(f"Create output directory {args.output_path}")
+    os.makedirs(args.output_path, exist_ok=True)
+
+    logger.info("Extract examples")
+    for start in trange(START_ITERATION, END_ITERATION, args.chunk_size):
+        end = min(start + args.chunk_size, END_ITERATION)
+        examples = []
+        for iteration in trange(start, end):
+            examples.extend(extract_batch(dataset, tokenizer, iteration))
+
+        output_path = os.path.join(args.output_path, f"pythia-{start:05d}-{end - 1:05d}.jsonl.gz")
+        logger.info(f"Write {len(examples)} examples to {output_path}")
+        with gzip.open(output_path, "wt", encoding="utf-8") as output_file:
+            for example in examples:
+                output_file.write(json.dumps(example, ensure_ascii=False) + "\n")
 
 
 if __name__ == "__main__":
