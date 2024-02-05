8367e34f3eb27fccb0c06be08b560bed0a2780d0
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2024-02-05 13:17:06 +0900

    add plot_approximate_memorization_ratio()

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.plot import plot_verbatim_memorization_ratio
+from src.plot import plot_approximate_memorization_ratio, plot_verbatim_memorization_ratio
 from src.utils import Example
 
 
@@ -81,3 +81,82 @@ class TestPlotVerbatimMemorizationRatio(unittest.TestCase):
             ),
         ]
         _ = plot_verbatim_memorization_ratio(examples)
+
+
+class TestPlotApproximateMemorizationRatio(unittest.TestCase):
+    def test_plot_approximate_memorization_ratio(self):
+        examples = [
+            Example(
+                iteration=0,
+                dataset_idx=0,
+                dataset_name="test",
+                doc_ids=[0],
+                text="test",
+                token_ids=[0],
+                metrics={
+                    "bleu/100": 0.5,
+                    "bleu/200": 0.6,
+                    "bleu/500": 0.7,
+                    "bleu/1000": 0.8,
+                },
+                completion_stats={
+                    "last_iteration": 0,
+                    "count": 1,
+                },
+            ),
+            Example(
+                iteration=0,
+                dataset_idx=0,
+                dataset_name="test",
+                doc_ids=[0],
+                text="test",
+                token_ids=[0],
+                metrics={
+                    "bleu/100": 0.5,
+                    "bleu/200": 0.6,
+                    "bleu/500": 0.7,
+                    "bleu/1000": 0.8,
+                },
+                completion_stats={
+                    "last_iteration": 0,
+                    "count": 1,
+                },
+            ),
+            Example(
+                iteration=1,
+                dataset_idx=0,
+                dataset_name="test",
+                doc_ids=[0],
+                text="test",
+                token_ids=[0],
+                metrics={
+                    "bleu/100": 0.5,
+                    "bleu/200": 0.6,
+                    "bleu/500": 0.7,
+                    "bleu/1000": 0.8,
+                },
+                completion_stats={
+                    "last_iteration": 1,
+                    "count": 1,
+                },
+            ),
+            Example(
+                iteration=1,
+                dataset_idx=0,
+                dataset_name="test",
+                doc_ids=[0],
+                text="test",
+                token_ids=[0],
+                metrics={
+                    "bleu/100": 0.5,
+                    "bleu/200": 0.6,
+                    "bleu/500": 0.7,
+                    "bleu/1000": 0.8,
+                },
+                completion_stats={
+                    "last_iteration": 1,
+                    "count": 1,
+                },
+            ),
+        ]
+        _ = plot_approximate_memorization_ratio(examples)
