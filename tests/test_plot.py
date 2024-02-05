d84b857d1ca5bff12a173b583e724edd2d94e19e
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2024-02-05 10:56:22 +0900

    update test_plot.py

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,105 +1,7 @@
 import unittest
 
-from src.plot import plot_extractable, plot_min_k_percent_prob, plot_perplexity
+from src.plot import plot_extractable
 from src.utils import Example
-
-
-class TestPlotPerplexity(unittest.TestCase):
-    def test_plot_perplexity(self):
-        examples = [
-            Example(
-                iteration=0,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={"perplexity": 1.0},
-            ),
-            Example(
-                iteration=0,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={"perplexity": 2.0},
-            ),
-            Example(
-                iteration=1,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={"perplexity": 2.0},
-            ),
-            Example(
-                iteration=1,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={"perplexity": 3.0},
-            ),
-        ]
-        _ = plot_perplexity(examples)
-
-
-class TestPlotMinKPercentProb(unittest.TestCase):
-    def test_plot_min_k_percent_prob(self):
-        examples = [
-            Example(
-                iteration=0,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={
-                    "min_k_percent_prob/20": 1.0,
-                    "min_k_percent_prob/30": 11.0,
-                },
-            ),
-            Example(
-                iteration=0,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={
-                    "min_k_percent_prob/20": 2.0,
-                    "min_k_percent_prob/30": 12.0,
-                },
-            ),
-            Example(
-                iteration=1,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={
-                    "min_k_percent_prob/20": 2.0,
-                    "min_k_percent_prob/30": 12.0,
-                },
-            ),
-            Example(
-                iteration=1,
-                dataset_idx=0,
-                dataset_name="test",
-                doc_ids=[0],
-                text="test",
-                token_ids=[0],
-                metrics={
-                    "min_k_percent_prob/20": 3.0,
-                    "min_k_percent_prob/30": 13.0,
-                },
-            ),
-        ]
-        _ = plot_min_k_percent_prob(examples)
 
 
 class TestPlotExtractable(unittest.TestCase):
@@ -113,8 +15,14 @@ class TestPlotExtractable(unittest.TestCase):
                 text="test",
                 token_ids=[0],
                 metrics={
-                    "extractable/20": 1.0,
-                    "extractable/30": 11.0,
+                    "extractable/100": 2.0,
+                    "extractable/200": 12.0,
+                    "extractable/500": 22.0,
+                    "extractable/1000": 32.0,
+                },
+                completion_stats={
+                    "last_iteration": 0,
+                    "count": 1,
                 },
             ),
             Example(
@@ -125,8 +33,14 @@ class TestPlotExtractable(unittest.TestCase):
                 text="test",
                 token_ids=[0],
                 metrics={
-                    "extractable/20": 2.0,
-                    "extractable/30": 12.0,
+                    "extractable/100": 2.0,
+                    "extractable/200": 12.0,
+                    "extractable/500": 22.0,
+                    "extractable/1000": 32.0,
+                },
+                completion_stats={
+                    "last_iteration": 0,
+                    "count": 1,
                 },
             ),
             Example(
@@ -137,8 +51,14 @@ class TestPlotExtractable(unittest.TestCase):
                 text="test",
                 token_ids=[0],
                 metrics={
-                    "extractable/20": 2.0,
-                    "extractable/30": 12.0,
+                    "extractable/100": 2.0,
+                    "extractable/200": 12.0,
+                    "extractable/500": 22.0,
+                    "extractable/1000": 32.0,
+                },
+                completion_stats={
+                    "last_iteration": 1,
+                    "count": 1,
                 },
             ),
             Example(
@@ -149,8 +69,14 @@ class TestPlotExtractable(unittest.TestCase):
                 text="test",
                 token_ids=[0],
                 metrics={
-                    "extractable/20": 3.0,
-                    "extractable/30": 13.0,
+                    "extractable/100": 2.0,
+                    "extractable/200": 12.0,
+                    "extractable/500": 22.0,
+                    "extractable/1000": 32.0,
+                },
+                completion_stats={
+                    "last_iteration": 1,
+                    "count": 1,
                 },
             ),
         ]
